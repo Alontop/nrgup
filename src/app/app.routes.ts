9ac81088197e69a7d6b58e087f8a0f5fdc31deda
alon.607 <alonsovivas2021@gmail.com>
2025-07-12 12:36:25 -0500

    Configuración de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'registro',
+        component: RegistroComponent,
+      },
+      {
+        path: '',
+        component: PantallaPrincipalComponent,
+      },
+      {
+        path: 'poto',
+        component: LoginComponent,
+      },
+    ],
+  },
+];
